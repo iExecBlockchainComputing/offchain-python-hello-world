de7823c03d98b3c4e1e426f99320af751c0bf8cd
maxence <maxence.cornaton@iex.ec>
2021-08-12 16:30:44 +0200

    Use sconify.args inside sconify.sh

--- offchain-computing/sconify.sh
+++ offchain-computing/sconify.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
+cd $(dirname $0)
+
+IMG_FROM=offchain-python-hello-world
+IMG_TO=offchain-python-hello-world:debug
+
+ARGS=$(sed -e "s'\${IMG_FROM}'${IMG_FROM}'" -e "s'\${IMG_TO}'${IMG_TO}'" sconify.args)
+echo $ARGS
 
 docker run -it --rm \
             -v /var/run/docker.sock:/var/run/docker.sock \
             registry.scontain.com:5050/scone-production/iexec-sconify-image:5.3.7 \
-            sconify_iexec \
-                --name=pythonHelloWorld \
-                --from=offchain-python-hello-world \
-                --to=tee-offchain-python-hello-world \
-                --binary-fs \
-                --fs-dir=/app \
-                --host-path=/etc/hosts \
-                --host-path=/etc/resolv.conf \
-                --binary="/usr/local/bin/python3.7" \
-                --heap="1G" \
-                --dlopen="2" \
-                --no-color \
-                --verbose \
-                --command="python3 /app/app.py"
+            sconify_iexec $ARGS
